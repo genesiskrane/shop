13d1d0c6ce2d0c577de20c9bad75594a134da06e
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-10 00:40:54 +0100

    Resync Added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-	<div>Vite Template</div>
+	<div>
+		<p>
+			App Name: Loading... <br />
+			{{ hostname }}
+		</p>
+	</div>
 </template>
+
+<script setup>
+const hostname = window.location.hostname;
+</script>
